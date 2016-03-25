4ea9cff9887b97f2b39b7e094f442b31f20f0395
sc0Vu <alk03073135@gmail.com>
2016-03-25 13:41:01 +0800

    something change

--- marquee.js
+++ marquee.js
@@ -3,86 +3,132 @@
  * email: alk03073135@gmail.com
  * Liscence: MIT
  */
-(function (d,a,b) {
-    var div = d.getElementById(a),
-        ul = d.getElementById(b),
-        ch = ul.children,
-        chl = ch.length;
-    if (chl === 0) {
-        ul.style.display = 'none';
-        return;
+(function (i,s,d) {
+    i[d] = function (id, opts) {
+    this._opts = opts || {};
+    this._marqueeParent = s.getElementById(id);
+    this._marqueeContainer = s.getElementById(this._opts.marqueeContainer);
+  }
+
+  i[d].prototype.init = function () {
+    if (this.checkCountEqual(0)) {
+      this._marqueeParent.style.display = 'none';
+      return false;
     }
-    var ofX = ch[chl-1].offsetLeft,
-        lW = ch[chl-1].offsetWidth,
-        time = (ofX + lW) / (chl * 10),
-        rule = [];
-    if (ofX < ul.clientWidth) {
-        var ww = (ul.clientWidth-10*chl) / chl;
-        for (var i=0; i<ch.length && ch.length>=1; i++) {
-            ch[i].style.width = ww+'px';
-            ch[i].style.textAlign = 'center';
-            ch[i].style.float = 'left';
-        }
-        if (chl === 1) {
-            ofX = ww;
-        } else {
-           ofX = chl * (ww+10); 
-        }
-    }
-    if ('animate' in ul) {
-        var marquee = ul.animate([
-            {
-                transform:'translate(0px)'
-            },
-            {
-                transform:'translate(-'+ofX+'px)'
-            },
-        ],{
-            duration: time*1000,
-            iterations: Infinity,
-            easing: 'ease',
-            direction: 'alternate',
-        }),
-        state = false;
-        marquee.play();
-        state = true
-        function stopMarquee() {
-            if (state === true) {
-                marquee.pause();
-                state = false
+    this._ch = this._marqueeContainer.children;
+    this._chl = this._ch.length;
+    this._liofX = this._ch[this._chl-1].offsetLeft;
+    this._liofW = this._ch[this._chl-1].offsetWidth;
+    this._cclW = this._marqueeContainer.clientWidth;
+    this._time = (this._liofX + this._liofW) / (this._chl * 10);
+    return true;
+  }
+
+  i[d].prototype.getScript = function () {
+    var r,
+        stys = s.styleSheets,
+        tmp = [];
+      
+    for (var i=0; i<stys.length; i++) {
+        for (var j=0; j<stys[i].cssRules.length; j++) {
+            r = stys[i].cssRules[j];
+            if (r.name === 'marquee' && r.type === CSSRule.KEYFRAMES_RULE) {
+                tmp.push(r);
             }
         }
-        function enableMarquee() {
-            if (state === false) {
-                marquee.play();
-                state = true;
-            }
-        }
-        ul.addEventListener('mouseenter', stopMarquee, false);
-        ul.addEventListener('mouseleave', enableMarquee, false);
-        return ;
     }
-    function getRule() {
-        var r,
-            s = d.styleSheets,
-            x = 0,
-            tmp = [];
-        for (var i=0; i<s.length; i++) {
-            for (var j=0; j<s[i].cssRules.length; j++) {
-                r = s[i].cssRules[j];
-                if (r.name == 'marquee' && r.type == CSSRule.KEYFRAMES_RULE) {
-                    tmp[x] = r;
-                    x++;
-                }
-            }
-        }
-        return tmp;
+    return tmp;
+  }
+
+  i[d].prototype.runMarqueeByAnimate = function () {
+    var self = this;
+    this._animation = this._marqueeContainer.animate([
+          {
+              transform:'translate(0px)'
+          }, {
+              transform:'translate(-'+this._liofX+'px)'
+          },
+      ], {
+          duration: this._time * 1000,
+          iterations: Infinity,
+          easing: 'ease',
+          direction: 'alternate',
+      });
+    this._marqueeContainer.onmouseenter = function () {
+      return self.stopMarquee();
     }
-    ul.style.animationDuration = ul.style.webkitAnimationDuration = time+'s';
-    rule = getRule();
+    this._marqueeContainer.onmouseleave = function () {
+      return self.resumeMarquee();
+    }
+    this.resumeMarquee();
+  }
+
+  i[d].prototype.stopMarquee = function () {
+    if (this._playState === true) {
+      this._animation.pause();
+      this._playState = false;
+    }
+  }
+  i[d].prototype.resumeMarquee = function () {
+    if (this._playState === false) {
+      this._animation.play();
+      this._playState = true;
+    }
+  }
+
+  i[d].prototype.runMarqueeByCSS = function () {
+    this._marqueeContainer.style.animationDuration = this._marqueeContainer.style.webkitAnimationDuration = this._time+'s';
+    var rule = this.getRule();
+  
     if (rule) {
-        for (var x of rule) {
-            x.appendRule("to {-ms-transform:translate("+(ofX)*-1+"px);-webkit-transform:translate("+(ofX)*-1+"px);transform:translate("+(ofX)*-1+"px);}");
-        }
+      rule.forEach( function (x) {
+        x.appendRule("to {-ms-transform:translate("+(this._liofX)*-1+"px);-webkit-transform:translate("+(this._liofX)*-1+"px);transform:translate("+(this._liofX)*-1+"px);}");
+      });
     }
-})(document, 'marquee', 'marquee-content');
+  }
+
+  i[d].prototype.countMarquee = function () {
+    return this._marqueeContainer.children.length;
+  }
+
+  i[d].prototype.isAnimate = function () {
+    return ('animate' in this._marqueeContainer);
+  }
+
+  i[d].prototype.checkCountEqual = function (num) {  
+    return (this._marqueeContainer.children.length === num) ? true : false;
+  }
+
+  i[d].prototype.checkClientWidth = function () {
+    if (this._liofX < this._cclW) {
+      let ww = (this._cclW-10*this._chl) / this._chl;
+
+      for (let i=0,l=this._ch.length; i<l && this._ch.length>=1; i++) {
+        this._ch[i].style.width = ww+'px';
+        this._ch[i].style.textAlign = 'center';
+        this._ch[i].style.float = 'left';
+      }
+
+      if (this._chl === 1) {
+        this._liofX = ww;
+      } else {
+        this._liofX = this._chl * (ww+10); 
+      }
+    }
+  }
+
+  i[d].prototype.run = function () {
+    if (!this.init()) {
+      return;
+    }
+
+    if (this.isAnimate()) {
+      this._playState = false;
+      this._animation = '';
+      this.runMarqueeByAnimate();
+    } else {
+      this.runMarqueeByCSS();
+    }
+    console.log(this);
+  }
+})(window, document, '$marquee$');
--- readme.md
+++ readme.md
@@ -1,1 +1,70 @@
-<h2>html5 marquee with <a href="https://w3c.github.io/web-animations/" target="_balnk">webanimation api</a>&nbsp;<a href="http://caniuse.com/#search=animate" target="_blank">caniuse</a><a href="https://jsfiddle.net/PeterLai/waevc3vc/" target="_blank">live example</a></h2>
+<h2>html5 marquee with <a href="https://w3c.github.io/web-animations/" target="_balnk">webanimation api</a></h2>
+<p>
+usage in HTML
+</p>
+<pre>
+<div id='a'>
+  <ul id='b'>
+    <li>Hello world!</li>
+    <li>Im running!#1</li>
+    <li>Im running!#2</li>
+    <li>Im running!#3</li>
+    <li>Im running!#4</li>
+    <li>Im running!#5</li>
+  </ul>
+</div>
+</pre>
+<p>
+usage in CSS
+</p>
+<pre>
+#a {
+    border: 1px #ccc dashed;
+    overflow: hidden;
+    max-height: 20px;
+    width: 100%;
+    position: absolute;
+}
+#b {
+    width: 100%;
+    max-height: 20px;
+    padding: 0;
+    position: relative;
+    white-space: nowrap;
+    top: 0;
+    margin: 0;
+    transition: 2s all ease;
+    /*-webkit-animation: 10s marquee ease infinite;*/
+    /*animation: 10s marquee ease infinite;*/
+}
+#b > li {
+    min-width: 10%;
+    min-height: 20px;
+    list-style: none;
+    display: inline-block;
+    position: relative;
+    left: auto;
+    padding: 1px 5px;
+}
+#b:hover {
+    animation-play-state: paused;
+}
+/*@-webkit-keyframes marquee {
+    from {text-indent:0;}
+}*/
+@keyframes marquee {
+    from {left:0;}
+}
+</pre>
+<p>
+usage in javascript
+</p>
+<pre>
+let marquee = new $marquee$(parentId, {marqueeContainer:marqueeContainerId});
+	marquee.run();
+</pre>
+<p>For animation api information and broswer support<br>
+<a href="https://w3c.github.io/web-animations/" target="_blank">w3c</a><br>
+<a href="http://caniuse.com/#search=animate" target="_blank">caniuse</a><br>
+<a href="https://developer.mozilla.org/zh-TW/docs/Web/API/Animation" target="_blank">MDN</a><br>
+<a href="https://jsfiddle.net/PeterLai/cL65vey3/23/" target="_blank">live example</a>
